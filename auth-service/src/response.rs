4628b72f83b8dd46275611ed8200cdccd4db43f8
Jose <jochristiannn@gmail.com>
2025-07-31 02:48:19 +0700

    Current User mechanism

--- auth-service/src/response.rs
+++ auth-service/src/response.rs
@@ -1,18 +1,5 @@
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
-#[derive(Serialize,Deserialize)]
-pub struct UserResponse{
-    pub id: Uuid,
-    pub name: String,
-    pub email: String,
-    pub dob: Option<chrono::NaiveDate>,
-    pub image: Option<String>,
-    pub role: String,
-    pub origin_id: Option<Uuid>,
-    pub created_at: chrono::NaiveDateTime,
-    pub updated_at: chrono::NaiveDateTime,
-}
+use model::users::UserResponse;
 
 #[derive(Serialize,Deserialize)]
 pub struct LoginResponse{
--- auth-service/src/routes.rs
+++ auth-service/src/routes.rs
@@ -1,4 +1,5 @@
 use axum::{Json, extract::State};
+use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use crate::{controller, AppState};
 use model::users::{NewUser,LoginRequest};
@@ -7,8 +8,12 @@ use crate::response::LoginResponse;
 pub async fn register(
     State(state): State<AppState>,
     Json(payload): Json<NewUser>,
-) -> Json<String> {
-    controller::register_user(payload,state).await
+) -> Response {
+    let (status, data) = controller::register_user(payload, state).await;
+
+    let mut response = data.into_response();
+    *response.status_mut() = status;
+    response
 }
 #[axum::debug_handler]
 pub async fn login(
@@ -21,8 +26,19 @@ pub async fn login(
     *response.status_mut() = status;
     response
 }
+
+pub async fn get_current_user(State(state): State<AppState>) -> Response {
+    let current = state.current_user.lock().await;
+    if let Some(user) = &*current {
+        Json(user).into_response()
+    } else {
+        (StatusCode::UNAUTHORIZED, "Not logged in").into_response()
+    }
+}
+
 pub fn auth_routes() -> axum::Router<AppState> {
     axum::Router::new()
         .route("/register", axum::routing::post(register))
         .route("/login", axum::routing::post(login))
+        .route("/get-user", axum::routing::get(get_current_user))
 }
